8da8c0a391bdd649b5e952c663fe967bf5b1c9c7
Benjamin <benjamin.guy.thomas@gmail.com>
2022-05-28 16:47:35 +0200

    Improve formatting

--- src/Main.elm
+++ src/Main.elm
@@ -166,9 +166,15 @@ view model =
             { title = "Home"
             , body =
                 [ nav_
-                , h1 [] [ text "Welcome!" ]
-                , p [] [ text "This is the home page." ]
-                , p [] [ text "To read posts, ", a [ href <| Route.path Route.ListPosts ] [ text "go here" ] ]
+                , main_ [ class "container" ]
+                    [ div [ class "row" ]
+                        [ div [ class "col s12 m6 l4" ]
+                            [ h1 [] [ text "Welcome!" ]
+                            , p [] [ text "This is the home page." ]
+                            , p [] [ text "To read posts, ", a [ href <| Route.path Route.ListPosts ] [ text "go here" ] ]
+                            ]
+                        ]
+                    ]
                 ]
             }
 
